916a09fd46973bec53ef321710316646a3e3d4e4
dimerbwimba <dimerbwimba191996@gmail.com>
2024-01-26 17:03:23 +0100

    Type error routes.ts

--- app/api/sections_single_blog/route.ts
+++ app/api/sections_single_blog/route.ts
@@ -2,26 +2,31 @@ import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
-export async function GET(req:Request){
-
+export async function GET(req: Request) {
     const client = await clientPromise;
     const db = client.db("test");
-    
-    const { searchParams } = new URL(req.url)
-    const id = searchParams.get('id')
-    const _id = new ObjectId(id)
-    try {
-        const sections = await db.collection("sections").find({
-            document: _id
-        },{
-            projection:{
-                _id:1,
-                title:1
-            }
-        }).toArray()
-        return NextResponse.json({success:true, sections:sections})
-    } catch (error) {
-        
-    }
 
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
+
+    if (id !== null) {
+        const _id = new ObjectId(id);
+
+        try {
+            const sections = await db.collection("sections").find({
+                document: _id
+            }, {
+                projection: {
+                    _id: 1,
+                    title: 1
+                }
+            }).toArray();
+
+            return NextResponse.json({ success: true, sections: sections });
+        } catch (error) {
+            // Handle error appropriately
+        }
+    } else {
+        // Handle the case where id is null (e.g., return an error response)
+    }
 }
