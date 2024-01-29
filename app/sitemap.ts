e7aaca68f977fd021b6ae8820279cbb9c269e199
dimerbwimba <dimerbwimba191996@gmail.com>
2024-01-29 06:59:54 +0100

    update the hero section

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,17 +2,24 @@ import axios from "axios";
 import { MetadataRoute } from "next";
 
 export default async function sitemap():Promise<MetadataRoute.Sitemap>{
-    const response = await axios.get(`https://explorewisetips.com/api/sitemap/blogs`)
-    const { documents } = await response.data
-
-    const blogEntries:MetadataRoute.Sitemap = documents.map(({slug, category_name, coverImage}:{slug:string, category_name:string, coverImage:string})=>({
-        url:`${process.env.NEXT_PUBLIC_BASE_URL}/${category_name}/${slug}`,
-        // lastModified: new Date(updated_at),
-        changeFrequency:"daily",
-        priority:1,
-    }))
-
-    return [
-        ...blogEntries
-    ]
+    try {
+        const response = await axios.get(`https://explorewisetips.com/api/sitemap/blogs`)
+        const { documents } = await response.data
+    
+        const blogEntries:MetadataRoute.Sitemap = documents.map(({slug, category_name, coverImage}:{slug:string, category_name:string, coverImage:string})=>({
+            url:`${process.env.NEXT_PUBLIC_BASE_URL}/${category_name}/${slug}`,
+            // lastModified: new Date(updated_at),
+            changeFrequency:"daily",
+            priority:1,
+        }))
+    
+        return [
+            ...blogEntries
+        ]
+        
+    } catch (error) {
+        return [
+            
+        ]
+    }
 }
